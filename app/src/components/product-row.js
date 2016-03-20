26c6b9def09dce692e61085e861c16d61598d5e3
Lin Liu <liulin1987315@gmail.com>
2016-03-20 19:06:40 +0800

    display the products that not in the stack in red color

--- app/src/components/product-row.js
+++ app/src/components/product-row.js
@@ -1,10 +1,14 @@
 import React from 'react';
 
-const ProductRow = ({product}) => (
-    <tr>
-      <td>{product.name}</td>
-      <td>{product.price}</td>
-    </tr>
-);
+const ProductRow = ({product}) => {
+    var name = product.stocked ? product.name : <span style={{color: 'red'}}> {product.name} </span>;
+
+    return (
+        <tr>
+          <td>{name}</td>
+          <td>{product.price}</td>
+        </tr>
+    );
+};
 
 export default ProductRow;
